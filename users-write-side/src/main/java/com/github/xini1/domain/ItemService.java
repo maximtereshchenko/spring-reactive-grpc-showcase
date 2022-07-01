9b8668050f67271d3b94b4c270044b734cbf5c5f
Xini1 <tereshchenko.xini@gmail.com>
2022-07-01 13:51:15 +0300

    Regular user cannot add new item for purchase test passes

--- users-write-side/src/main/java/com/github/xini1/domain/ItemService.java
+++ users-write-side/src/main/java/com/github/xini1/domain/ItemService.java
@@ -1,5 +1,6 @@
 package com.github.xini1.domain;
 
+import com.github.xini1.exception.UserIsNotAdmin;
 import com.github.xini1.usecase.AddItemUseCase;
 import com.github.xini1.usecase.EventStore;
 import com.github.xini1.usecase.Identifiers;
@@ -23,8 +24,9 @@ final class ItemService implements AddItemUseCase {
 
     @Override
     public void addItem(UUID userId, User user, String name) {
-        if (user == User.ADMIN) {
-            eventStore.publish(new ItemAdded(userId, identifiers.newIdentifier(), name));
+        if (user != User.ADMIN) {
+            throw new UserIsNotAdmin();
         }
+        eventStore.publish(new ItemAdded(userId, identifiers.newIdentifier(), name));
     }
 }
