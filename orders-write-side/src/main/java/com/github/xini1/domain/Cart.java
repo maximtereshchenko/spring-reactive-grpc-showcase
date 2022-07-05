a824f59dce9c4e3a66447bbb340e9322d81ea773
Xini1 <tereshchenko.xini@gmail.com>
2022-07-05 10:05:40 +0300

    'User cannot remove item from cart, if quantity is more than cart has' test passes

--- orders-write-side/src/main/java/com/github/xini1/domain/Cart.java
+++ orders-write-side/src/main/java/com/github/xini1/domain/Cart.java
@@ -5,6 +5,7 @@ import com.github.xini1.event.cart.ItemsOrdered;
 import com.github.xini1.exception.CartHasDeactivatedItem;
 import com.github.xini1.exception.CartIsEmpty;
 import com.github.xini1.exception.CouldNotAddDeactivatedItemToCart;
+import com.github.xini1.exception.QuantityIsMoreThanCartHas;
 import com.github.xini1.exception.QuantityIsNotPositive;
 import com.github.xini1.port.EventStore;
 
@@ -46,6 +47,9 @@ final class Cart extends AggregateRoot {
     void remove(Item item, int quantity) {
         if (quantity < 1) {
             throw new QuantityIsNotPositive();
+        }
+        if (quantity > items.getOrDefault(item.id(), 0)) {
+            throw new QuantityIsMoreThanCartHas();
         }
     }
 
