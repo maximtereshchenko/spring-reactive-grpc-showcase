fee0f52692c9c475246f45eac97a919ec3e23d7c
Xini1 <tereshchenko.xini@gmail.com>
2022-07-06 14:53:16 +0300

    'Regular user cannot view top ordered items' test

--- orders-read-side/src/test/java/com/github/xini1/ViewTopOrderedItemsUseCaseTest.java
+++ orders-read-side/src/test/java/com/github/xini1/ViewTopOrderedItemsUseCaseTest.java
@@ -1,8 +1,10 @@
 package com.github.xini1;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.github.xini1.domain.Module;
+import com.github.xini1.exception.UserIsNotAdmin;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -15,6 +17,13 @@ final class ViewTopOrderedItemsUseCaseTest {
     private final Module module = new Module(new InMemoryViewStore());
     private final UUID userId = UUID.fromString("00000000-000-0000-0000-000000000001");
     private final UUID itemId = UUID.fromString("00000000-000-0000-0000-000000000002");
+
+    @Test
+    void givenUserIsNotAdmin_whenViewTopOrderedItems_thenUserIsNotAdminThrown() {
+        var useCase = module.viewTopOrderedItemsUseCase();
+
+        assertThatThrownBy(() -> useCase.view(User.REGULAR)).isInstanceOf(UserIsNotAdmin.class);
+    }
 
     @Test
     void givenNoItemsOrdered_whenViewTopOrderedItems_thenIterableIsEmpty() {
