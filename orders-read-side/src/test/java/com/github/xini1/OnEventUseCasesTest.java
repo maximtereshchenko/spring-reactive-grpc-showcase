277a4bcd0d3683f15da6bb16ea5a28b7d4bcf207
Xini1 <tereshchenko.xini@gmail.com>
2022-07-06 09:23:44 +0300

    'Nonexistent item cannot be removed from cart' test passes

--- orders-read-side/src/test/java/com/github/xini1/OnEventUseCasesTest.java
+++ orders-read-side/src/test/java/com/github/xini1/OnEventUseCasesTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.github.xini1.domain.Module;
 import com.github.xini1.event.cart.ItemAddedToCart;
+import com.github.xini1.event.cart.ItemRemovedFromCart;
 import com.github.xini1.exception.ItemIsNotFound;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ import java.util.UUID;
 /**
  * @author Maxim Tereshchenko
  */
-final class OnItemAddedToCartEventUseCaseTest {
+final class OnEventUseCasesTest {
 
     private final Module module = new Module(new InMemoryViewStore());
     private final UUID userId = UUID.fromString("00000000-000-0000-0000-000000000001");
@@ -22,6 +23,15 @@ final class OnItemAddedToCartEventUseCaseTest {
     void givenItemDoNotExist_whenOnItemAddedToCartEvent_thenItemIsNotFoundThrown() {
         var useCase = module.onItemAddedToCartEventUseCase();
         var itemAddedToCart = new ItemAddedToCart(1, userId, itemId, 1);
+
+        assertThatThrownBy(() -> useCase.onEvent(itemAddedToCart))
+                .isInstanceOf(ItemIsNotFound.class);
+    }
+
+    @Test
+    void givenItemDoNotExist_whenOnItemRemovedFromCartEvent_thenItemIsNotFoundThrown() {
+        var useCase = module.onItemRemovedFromCartEventUseCase();
+        var itemAddedToCart = new ItemRemovedFromCart(1, userId, itemId, 1);
 
         assertThatThrownBy(() -> useCase.onEvent(itemAddedToCart))
                 .isInstanceOf(ItemIsNotFound.class);
