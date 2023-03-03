a1dd511f82cd25380aa6dd4a7a15ffe0dfcb3d26
Maxim Tereshchenko <maksimt@backbase.com>
2023-03-03 09:04:52 +0100

    Update certificates

--- certificates.sh
+++ certificates.sh
@@ -6,8 +6,8 @@ openssl genrsa -aes256 -passout pass:password -out ca-private-key.key 4096
 openssl req -new -key ca-private-key.key -passin pass:password -subj "/CN=Certificate authority/" -out ca-certificate-signing-request.csr
 openssl x509 -req -in ca-certificate-signing-request.csr -signkey ca-private-key.key -passin pass:password -days 365 -out ca-self-signed-certificate.pem
 openssl genrsa -aes256 -out server-private-key.key -passout pass:password 4096
-openssl req -new -key server-private-key.key -passin pass:password -subj "/CN=spring-reactive-grpc-showcase/" -addext "subjectAltName=DNS:*.amazonaws.com,DNS:localhost,DNS:users,DNS:orders-read-side,DNS:orders-write-side" -out server-certificate-signing-request.csr
-openssl x509 -req -in server-certificate-signing-request.csr -CA ca-self-signed-certificate.pem -CAkey ca-private-key.key -passin pass:password -CAcreateserial -days 365 -out server-certificate.pem -extfile <(echo "subjectAltName=DNS:*.amazonaws.com,DNS:localhost,DNS:users,DNS:orders-read-side,DNS:orders-write-side")
+openssl req -new -key server-private-key.key -passin pass:password -subj "/CN=*.us-east-1.elb.amazonaws.com/" -addext "subjectAltName=DNS:localhost,DNS:users,DNS:orders-read-side,DNS:orders-write-side" -out server-certificate-signing-request.csr
+openssl x509 -req -in server-certificate-signing-request.csr -CA ca-self-signed-certificate.pem -CAkey ca-private-key.key -passin pass:password -CAcreateserial -days 365 -out server-certificate.pem -extfile <(echo "subjectAltName=DNS:localhost,DNS:users,DNS:orders-read-side,DNS:orders-write-side")
 openssl rsa -in server-private-key.key -passin pass:password -out server-private-key-no-pass.key
 openssl pkcs8 -topk8 -nocrypt -in server-private-key-no-pass.key -out server-private-key-pkcs8.key
 
